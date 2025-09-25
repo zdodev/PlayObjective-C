14806310a4761eb0bccfb38d5cb91ecf451fd87e
zdodev <iver0945@gmail.com>
2025-09-25 11:02:43 +0900

    ✨: NS_ENUM 정리

---
+++ Introduction/DataTypes/Enum/Direction.h
@@ -0,0 +1,11 @@
+#ifndef Direction_h
+#define Direction_h
+
+typedef NS_ENUM(NSInteger, Direction) {
+    North,
+    East,
+    West,
+    South
+};
+
+#endif
--- Introduction/DataTypes/main.m
+++ Introduction/DataTypes/main.m
@@ -1,10 +1,29 @@
 #import <Foundation/Foundation.h>
 #import "DataTypeClass.h"
+#import "Direction.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         DataTypeClass *dataTypeClass = [DataTypeClass new];
         [dataTypeClass printTypes];
+        
+        Direction direction = South;
+        switch (direction) {
+            case North:
+                NSLog(@"북쪽입니다.");
+                break;
+            case East:
+                NSLog(@"동쪽입니다.");
+                break;
+            case West:
+                NSLog(@"서쪽입니다.");
+                break;
+            case South:
+                NSLog(@"남쪽입니다.");
+                break;
+            default:
+                break;
+        }
     }
     return EXIT_SUCCESS;
 }
